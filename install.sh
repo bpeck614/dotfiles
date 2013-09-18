d60c49dce1688feb965920223651df1441ac8780
Brian Peck <bpeck@iastate.edu>
2013-09-18 15:00:16 -0500

    Initial commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dir=~/dotfiles # dotfiles directorya
+olddir=~/dotfiles_old # old dotfiles backup directory`
+files="bashrc" # list of files to symlink in home
+
+mkdir -p $olddir
+cd $dir
+
+for file in $files
+do
+	mv ~/.$file ~/dotfiles_old/
+	ln -s $dir/$file ~/.$file
+done
+
