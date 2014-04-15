c812eb11b9234474bef9276fc3e96f984689946f
Brian Peck <bpeck@iastate.edu>
2014-04-15 14:28:14 -0500

    Add latexmkrc to autostart evince

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 dir=~/dotfiles # dotfiles directorya
 olddir=~/dotfiles_old # old dotfiles backup directory`
-files="bashrc vimrc tmux.conf" # list of files to symlink in home
+files="bashrc vimrc tmux.conf latexmkrc" # list of files to symlink in home
 
 mkdir -p $olddir
 cd $dir
